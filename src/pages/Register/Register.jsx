67468bcfe6d8cb215140b786a6c3b66c34d61a41
Harihar Padhi <39830449+1515236544hit@users.noreply.github.com>
2023-10-21 22:33:07 +0530

    complete the component structure for steps of verfication

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 // import styles from "./Register.module.css";
+import StepPhoneEmail from "../Steps/StepPhoneEmail/StepPhoneEmail";
+import StepOtp from "../Steps/StepOtp/StepOtp";
+import StepName from "../Steps/StepName/StepName";
+import StepAvatar from "../Steps/StepAvatar/StepAvatar";
+import StepUserName from "../Steps/StepUserName/StepUserName";
+
+const steps = {
+  1: StepPhoneEmail,
+  2: StepOtp,
+  3: StepName,
+  4: StepAvatar,
+  5: StepUserName,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [step, setStep] = useState(2);
+  const Step = steps[step];
+  return (
+    <div>
+      <Step />
+    </div>
+  );
 };
 
 export default Register;
